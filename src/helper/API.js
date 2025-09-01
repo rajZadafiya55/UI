f746b1beeead7c342252b63383e224bc5db63330
rajZadafiya55 <zadagfiyaraj395.com>
2025-09-01 21:46:43 +0530

    ads

--- src/helper/API.js
+++ src/helper/API.js
@@ -1,1 +1,2 @@
-export const APIHttp = "https://server-api-3ic1.onrender.com/api";
+// export const APIHttp = "https://server-api-3ic1.onrender.com/api";
+export const APIHttp = "http://localhost:5000/api";
